ac4275617ffadd2ae3ff5e2e46fbe2ff3916e8ea
cezklosowski <cez.klosowski@gmail.com>
2020-07-26 10:21:14 +0200

    DataPrivider - static and return optional QueryThread - use thread in while loop to get data from ZTM

--- src/main/java/com/sda/cezklosowski/ztmstats/inbound/DataProvider.java
+++ src/main/java/com/sda/cezklosowski/ztmstats/inbound/DataProvider.java
@@ -3,21 +3,22 @@ package com.sda.cezklosowski.ztmstats.inbound;
 import com.sda.cezklosowski.ztmstats.model.ZtmData;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class DataProvider {
 
     private static final String URL = "https://ckan2.multimediagdansk.pl/gpsPositions";
 
-    public ZtmData getZtmData() {
+    public static Optional<ZtmData> getZtmData() {
         String rawJson = null;
         try {
             rawJson = ZtaRawDataProvider.requestCurrentData(URL);
         } catch (IOException e) {
             System.out.println("Error getting data from " + URL);
             e.printStackTrace();
-            return null;
+            return Optional.empty();
         }
-        return DataMapper.mapJsonToZtmData(rawJson);
+        return Optional.ofNullable(DataMapper.mapJsonToZtmData(rawJson));
     }
 
 }
---
+++ src/main/java/com/sda/cezklosowski/ztmstats/inbound/QueryThread.java
@@ -0,0 +1,39 @@
+package com.sda.cezklosowski.ztmstats.inbound;
+
+import com.sda.cezklosowski.ztmstats.core.DataStorage;
+import com.sda.cezklosowski.ztmstats.model.ZtmData;
+
+import java.time.LocalTime;
+import java.util.Optional;
+
+public class QueryThread extends Thread {
+    private DataStorage dataStorage;
+
+    public QueryThread(DataStorage dataStorage) {
+        this.dataStorage = dataStorage;
+    }
+
+    @Override
+    public synchronized void start() {
+        System.out.println(getName() + " has started at - " + LocalTime.now());
+        super.start();
+    }
+
+    @Override
+    public void run() {
+
+        while (true) {
+            Optional<ZtmData> optionalOfZtmData = DataProvider.getZtmData();
+            optionalOfZtmData.ifPresentOrElse(
+                    ztmData -> dataStorage.update(ztmData),
+                    () -> System.out.println("No ZTA data retrived!"));
+            try {
+                Thread.sleep(30_000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
